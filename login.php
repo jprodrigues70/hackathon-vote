17c3430905ba4203ed5173f2a5ed2cf4fc1d0dbd
JP Rodrigues <70jprodrigues@gmail.com>
2017-06-02 17:19:42 -0300

    Adiciona página de login

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hackathon - UFBA</title>
+    <link rel="stylesheet" href="bower_components/gaintime/css/gaintime.min.css" media ="screen" title="no title">
+    <link rel="stylesheet" href="assets/css/hackathon.min.css" media ="screen" title="no title">
+  </head>
+  <body class="panel">
+    <section class="login-card">
+      <div class="text-center">
+          <img class="brand" src="assets/img/marca.png" alt="Desafios Bahia Hackathon" title="Desafios Bahia Hackathon" />
+      </div>
+      <form action="" method="POST" class="gt-form">
+        <input placeholder="Username" id="username" type="text" name="username" required>
+        <button type="submit" class="btn success center">Entrar</button>
+      </form>
+    </section>
+    <script type="text/javascript" src="bower_components/gaintime/js/gaintime.min.js"></script>
+  </body>
+</html>
