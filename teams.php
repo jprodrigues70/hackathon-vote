b757ef1f6c52e03766fecc33f21e7d1997ce4eb9
JP Rodrigues <jprodrigues70@gmail.com>
2017-06-03 12:25:24 -0300

    Corrige exibição de times

--- teams.php
+++ teams.php
@@ -11,10 +11,42 @@
     <section class="container">
       <?php Session::msg(); ?>
       <h1>Equipes</h1>
-      <div class="content-align">
+      <div class="flex-box">
 
           <?php foreach ($teams as $key => $team): ?>
-          <div class="card" data-modal="team<?php Prints::it($team, 'id'); ?>">
+          <div class="card col" data-modal="team<?php Prints::it($team, 'id'); ?>">
+              <h3><?php Prints::it($team, 'name') ?></h3>
+              <ul>
+                <?php foreach ($competitors as $key => $competitor): ?>
+                    <?php if ($team->id == $competitor->teams_id): ?>
+                        <li><?php Prints::it($competitor, 'name') ?></li>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+              </ul>
+          </div>
+          <div class="gt-modal">
+              <div id="team<?php Prints::it($team, 'id'); ?>" class="modal">
+                  <button class="modal-close" type="button" name="button">×</button>
+                  <div class="modal-header">
+                      <h1><?php Prints::it($team, 'name') ?></h1>
+                  </div>
+                  <div class="modal-body">
+                      <form class="gt-form text-center" action="controllers/vote_controller" method="post">
+                          <input type="hidden" name="teams_id" value="<?php Prints::it($team, 'id'); ?>">
+                          <?php foreach ($criterias as $key => $criteria): ?>
+                              <input type="hidden" name="criterias_id[]" value="<?php Prints::it($criteria, 'id'); ?>">
+                              <label class="text-left" for="c<?php Prints::it($criteria, 'id') ?>">
+                                  <h3><?php Prints::it($criteria, 'title') ?></h3>
+                              </label>
+                              <input data-mirror="c<?php Prints::it($criteria, 'id') ?>" type="range" name="grade[]" min="2" max="10" oninput="">
+                              <span id="c<?php Prints::it($criteria, 'id') ?>" class="tag info">0</span>
+                          <?php endforeach; ?>
+                          <button class="btn success full" type="submit" name="action" value="store">VOTAR</button>
+                      </form>
+                  </div>
+              </div>
+          </div>
+          <div class="card col" data-modal="team<?php Prints::it($team, 'id'); ?>">
               <h3><?php Prints::it($team, 'name') ?></h3>
               <ul>
                 <?php foreach ($competitors as $key => $competitor): ?>
