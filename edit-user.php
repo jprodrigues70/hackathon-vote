cbe2629205db1beadcdb7f75af5ea13317c5d284
Gildásio Júnior <gjuniioor@protonmail.com>
2017-06-03 16:59:50 -0300

    Adiciona painel de usuário

---
+++ edit-user.php
@@ -0,0 +1,23 @@
+<?php require_once('heart/pulse.php'); ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hackathon - UFBA</title>
+    <link rel="stylesheet" href="bower_components/gaintime/css/gaintime.min.css" media ="screen" title="no title">
+    <link rel="stylesheet" href="assets/css/hackathon.min.css" media ="screen" title="no title">
+  </head>
+  <body class="panel">
+      <?php include_once('includes/_headeradmin.inc') ?>
+      <section>
+        <div class="top-bar">
+          <h2>Editar Usuário</h2>
+        </div>
+        <?php include_once('includes/forms/user.inc') ?>
+      </section>
+      <?php include_once('includes/_footer.inc') ?>
+    <script type="text/javascript" src="bower_components/gaintime/js/gaintime.min.js"></script>
+  </body>
+</html>
---
+++ users.php
@@ -0,0 +1,41 @@
+<?php require_once('heart/pulse.php'); ?>
+<?php Session::is_up(); ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php include_once('includes/_head.inc') ?>
+  </head>
+  <body>
+    <?php include_once('includes/_headeradmin.inc') ?>
+    <section class="container">
+      <?php Session::msg(); ?>
+      <div class="top-bar">
+          <h1>Usuários</h1>
+          <a href="new-user" class="btn btn-add">Adicionar</a>
+      </div>
+      <table class="gt-table">
+          <thead>
+              <tr>
+                  <th>Username</th>
+                  <th>Ações</th>
+              </tr>
+          </thead>
+          <tbody class="text-center">
+              <?php foreach ($users as $key => $user): ?>
+              <tr>
+                  <td><?php Prints::it($user, 'name') ?></td>
+                  <td>
+                    <a class="action info" href="edit-user<?php Prints::it($user, 'id', 'get/id') ?>">Editar</a>
+                    <a data-ask="Tem certeza que deseja excluir o usuário <?php Prints::it($user, 'name'); ?>?" class="action danger" href="controllers/user_controller<?php Prints::it($user, 'id', 'get/delete') ?>">Excluir</a>
+                  </td>
+              </tr>
+            <?php endforeach; ?>
+        </tbody>
+      </table>
+    </section>
+
+    <?php include_once('includes/_footer.inc') ?>
+    <script type="text/javascript" src="bower_components/gaintime/js/gaintime.min.js"></script>
+    <script type="text/javascript" src="assets/js/hackathon.js"></script>
+  </body>
+</html>
