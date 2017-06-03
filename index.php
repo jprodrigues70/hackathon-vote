724f31dace7cad5360d01298475231efa99ee790
JP Rodrigues <jprodrigues70@gmail.com>
2017-06-03 14:29:37 -0300

    Exibe componentes ao clicar em equipe no ranking

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php $contexts = ['team', 'criteria']; ?>
+<?php $contexts = ['team', 'criteria', 'competitor']; ?>
 <?php require_once('heart/pulse.php'); ?>
 <!DOCTYPE html>
 <html>
@@ -21,7 +21,7 @@
         </thead>
         <tbody>
         <?php foreach ($results as $key => $result): ?>
-            <tr>
+            <tr data-modal="team<?php Prints::it($result, 'id'); ?>">
                 <td>
                     <?php if ($key < 3): ?>
                         <img src="assets/img/minilogo.png" alt="Lâmpada" <?php $tm->getMedal($key); ?>>
@@ -32,11 +32,30 @@
                 <div class="bar" data-percentage="<?php echo (($result->result*10/$criteria_size)); ?>%" data-color="success" data-tooltip="<?php Prints::it($result, 'result') ;?> pts" data-text="<?php Prints::it($result, 'result') ;?> pts">
                 </td>
             </tr>
+            <div class="gt-modal">
+                <div id="team<?php Prints::it($result, 'id'); ?>" class="modal">
+                    <button class="modal-close" type="button" name="button">×</button>
+                    <div class="modal-header">
+                        <h1><?php Prints::it($result, 'name') ?></h1>
+
+                        <ul>
+                          <?php foreach ($competitors as $key => $competitor): ?>
+                              <?php if ($result->id == $competitor->teams_id): ?>
+                                  <li><?php Prints::it($competitor, 'name') ?></li>
+                              <?php endif; ?>
+                          <?php endforeach; ?>
+                        </ul>
+                    </div>
+                    <div class="modal-body">
+                    </div>
+                </div>
+            </div>
         <?php endforeach; ?>
         </tbody>
       </table>
     </section>
     <?php include_once('includes/_footer.inc') ?>
     <script type="text/javascript" src="bower_components/gaintime/js/gaintime.min.js"></script>
+    <script type="text/javascript" src="assets/js/hackathon.js"></script>
   </body>
 </html>
